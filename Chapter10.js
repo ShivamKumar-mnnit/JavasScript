5b4d25981d9cc24083fcf2aaa219971c234ecaa1
Mickey <mickeyth59@gmail.com>
2024-01-04 17:14:25 +0530

    API and CurrencyConvertor

---
+++ Chapter10.js
@@ -0,0 +1,18 @@
+//fetch API: application programming interface
+
+console.log("Chapter10");
+// The fetch api provides an interface for fetching resources
+// let promise = fetch(url,[options]);
+
+//Understanding terms
+//AJAX: is async js and xml. all apis are requestion through ajax
+//json: is javascrips object notation it is easier to understand and manage so we use json over AJAX 
+//res.json();
+
+//HTTP: hyper text transfer protocol
+//it has req methods like get post delete put etc;
+//response has its status and code can be read by mdm
+//success code : 200;
+
+
+//more learning via projects 
